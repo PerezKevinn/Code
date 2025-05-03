bc624421c0581759bb1848972404314b996833b9
Kevin Pérez <163857043+PerezKevinn@users.noreply.github.com>
2025-05-03 13:46:23 -0500

    Updates

--- veterinaria/lib/screen/home_page.dart
+++ veterinaria/lib/screen/home_page.dart
@@ -8,8 +8,28 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     return Scaffold(
-      backgroundColor: const Color(0xFFF0ECE8),
-      body: Stack(children: [
+      backgroundColor: Colors.red,
+      body: Stack(
+        children: [
+          Positioned(
+            left: size.width * 0.02,
+            top: size.height * 0.052,
+            right: size.width * 0.02,
+            child: Container(
+              height: size.height * 0.25,
+              width: size.width * 0.8,
+              decoration: BoxDecoration(
+                color: const Color(0xFFFFFFFF),
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Center(child: Image.asset('assets/avatars/Avatar_01.png')),
+            ),
+          ),
+          Positioned(
+            left: size.width * 0.02,
+            top: size.height * 0.3,
+            child: Image.asset('assets/icons/Huela_32px.png'),
+          ),
         ],
       ),
     );
